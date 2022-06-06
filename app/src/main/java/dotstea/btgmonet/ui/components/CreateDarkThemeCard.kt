c421f43ff9baa911628cb79f8de1db90ef21afdb
Dot's Tea Lab <dotstealab@gmail.com>
2022-06-06 12:22:12 +0200

    separated themes, added back old color scheme system for testing purposes, changed theme values to new system color scheme. seems to be working so far

--- app/src/main/java/dotstea/btgmonet/ui/components/CreateDarkThemeCard.kt
+++ app/src/main/java/dotstea/btgmonet/ui/components/CreateDarkThemeCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import dotstea.btgmonet.R
+import dotstea.btgmonet.ui.theme.DarkTheme
 import java.io.File
 import androidx.compose.material3.MaterialTheme.colorScheme as monet
 
@@ -20,9 +21,15 @@ import androidx.compose.material3.MaterialTheme.colorScheme as monet
 @Composable
 fun DarkThemeCard(shareTheme: () -> Unit,  application: Application, applicationContext: Context) {
     var createDarkThemeState by remember { mutableStateOf(false) }
-    if (!createDarkThemeState) else createDarkTheme(shareTheme, application, applicationContext)
 
-    Card(Modifier.padding(32.dp).fillMaxWidth(), RoundedCornerShape(32.dp)) {
+    DarkTheme() {
+        if (!createDarkThemeState) else createDarkTheme(shareTheme, application, applicationContext)
+    }
+
+    Card(
+        Modifier
+            .padding(32.dp)
+            .fillMaxWidth(), RoundedCornerShape(32.dp)) {
         Button(onClick = { createDarkThemeState =! createDarkThemeState }) {
             Text(text = "Create Dark Theme")
         }
@@ -33,7 +40,7 @@ fun DarkThemeCard(shareTheme: () -> Unit,  application: Application, application
 fun createDarkTheme(shareTheme: () -> Unit, application: Application, applicationContext: Context) {
     val darkMonetFile = "monet_dark.attheme"
     val darkThemeImport = application.assets.open(darkMonetFile).bufferedReader().readText()
-    val themeString = changeText(darkThemeImport)
+    val themeString = changeText(darkThemeImport, applicationContext)
     val fileName = "Dark Monet Theme.attheme"
 
     File(applicationContext.cacheDir, fileName).writeText(text = themeString)
@@ -41,7 +48,7 @@ fun createDarkTheme(shareTheme: () -> Unit, application: Application, applicatio
 }
 
 @Composable
-fun changeText(file: String): String {
+fun changeText(file: String, applicationContext: Context): String {
     val monetList = mapOf(
         "surface" to monet.surface.toArgb(),
         "surfaceTint" to monet.surfaceTint.toArgb(),
@@ -68,6 +75,72 @@ fun changeText(file: String): String {
         "onErrorContainer" to monet.onErrorContainer.toArgb(),
         "outline" to monet.outline.toArgb(),
         "none" to Color.Transparent.toArgb(),
+
+        "a1_0" to ContextCompat.getColor(applicationContext, R.color.system_accent1_0),
+        "a1_200" to ContextCompat.getColor(applicationContext, R.color.system_accent1_200),
+        "a1_300" to ContextCompat.getColor(applicationContext, R.color.system_accent1_300),
+        "a1_400" to ContextCompat.getColor(applicationContext, R.color.system_accent1_400),
+        "a1_500" to ContextCompat.getColor(applicationContext, R.color.system_accent1_500),
+        "a1_600" to ContextCompat.getColor(applicationContext, R.color.system_accent1_600),
+        "a1_700" to ContextCompat.getColor(applicationContext, R.color.system_accent1_700),
+        "a1_800" to ContextCompat.getColor(applicationContext, R.color.system_accent1_800),
+        "a1_900" to ContextCompat.getColor(applicationContext, R.color.system_accent1_900),
+        "a1_1000" to ContextCompat.getColor(applicationContext, R.color.system_accent1_1000),
+        "a1_100" to ContextCompat.getColor(applicationContext, R.color.system_accent1_100),
+        "a1_10" to ContextCompat.getColor(applicationContext, R.color.system_accent1_10),
+        "a1_50" to ContextCompat.getColor(applicationContext, R.color.system_accent1_50),
+        "a2_0" to ContextCompat.getColor(applicationContext, R.color.system_accent2_0),
+        "a2_200" to ContextCompat.getColor(applicationContext, R.color.system_accent2_200),
+        "a2_300" to ContextCompat.getColor(applicationContext, R.color.system_accent2_300),
+        "a2_400" to ContextCompat.getColor(applicationContext, R.color.system_accent2_400),
+        "a2_500" to ContextCompat.getColor(applicationContext, R.color.system_accent2_500),
+        "a2_600" to ContextCompat.getColor(applicationContext, R.color.system_accent2_600),
+        "a2_700" to ContextCompat.getColor(applicationContext, R.color.system_accent2_700),
+        "a2_800" to ContextCompat.getColor(applicationContext, R.color.system_accent2_800),
+        "a2_900" to ContextCompat.getColor(applicationContext, R.color.system_accent2_900),
+        "a2_1000" to ContextCompat.getColor(applicationContext, R.color.system_accent2_1000),
+        "a2_100" to ContextCompat.getColor(applicationContext, R.color.system_accent2_100),
+        "a2_10" to ContextCompat.getColor(applicationContext, R.color.system_accent2_10),
+        "a2_50" to ContextCompat.getColor(applicationContext, R.color.system_accent2_50),
+        "a3_0" to ContextCompat.getColor(applicationContext, R.color.system_accent3_0),
+        "a3_200" to ContextCompat.getColor(applicationContext, R.color.system_accent3_200),
+        "a3_300" to ContextCompat.getColor(applicationContext, R.color.system_accent3_300),
+        "a3_400" to ContextCompat.getColor(applicationContext, R.color.system_accent3_400),
+        "a3_500" to ContextCompat.getColor(applicationContext, R.color.system_accent3_500),
+        "a3_600" to ContextCompat.getColor(applicationContext, R.color.system_accent3_600),
+        "a3_700" to ContextCompat.getColor(applicationContext, R.color.system_accent3_700),
+        "a3_800" to ContextCompat.getColor(applicationContext, R.color.system_accent3_800),
+        "a3_900" to ContextCompat.getColor(applicationContext, R.color.system_accent3_900),
+        "a3_1000" to ContextCompat.getColor(applicationContext, R.color.system_accent3_1000),
+        "a3_100" to ContextCompat.getColor(applicationContext, R.color.system_accent3_100),
+        "a3_10" to ContextCompat.getColor(applicationContext, R.color.system_accent3_10),
+        "a3_50" to ContextCompat.getColor(applicationContext, R.color.system_accent3_50),
+        "n1_0" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_0),
+        "n1_200" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_200),
+        "n1_300" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_300),
+        "n1_400" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_400),
+        "n1_500" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_500),
+        "n1_600" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_600),
+        "n1_700" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_700),
+        "n1_800" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_800),
+        "n1_900" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_900),
+        "n1_1000" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_1000),
+        "n1_100" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_100),
+        "n1_10" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_10),
+        "n1_50" to ContextCompat.getColor(applicationContext, R.color.system_neutral1_50),
+        "n2_0" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_0),
+        "n2_200" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_200),
+        "n2_300" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_300),
+        "n2_400" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_400),
+        "n2_500" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_500),
+        "n2_600" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_600),
+        "n2_700" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_700),
+        "n2_800" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_800),
+        "n2_900" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_900),
+        "n2_1000" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_1000),
+        "n2_100" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_100),
+        "n2_10" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_10),
+        "n2_50" to ContextCompat.getColor(applicationContext, R.color.system_neutral2_50)
     )
 
     var themeText = file.replace("\$", "")
--- app/src/main/java/dotstea/btgmonet/ui/theme/Theme.kt
+++ app/src/main/java/dotstea/btgmonet/ui/theme/Theme.kt
@@ -2,14 +2,18 @@ package dotstea.btgmonet.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -44,14 +48,66 @@ fun BTGMonetTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    val colorSchemeDark = dynamicDarkColorScheme(context)
+    val colorSchemeLight = dynamicLightColorScheme(context)
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+@Composable
+fun DarkTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val colorScheme = dynamicDarkColorScheme(context)
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+@Composable
+fun LightTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val colorScheme = dynamicLightColorScheme(context)
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
